a963f9f7c3ad083bf25b4976239852c49654f176
deeniee <dev.beeniee@gmail.com>
2025-05-14 02:59:08 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -0,0 +1,8 @@
+// Q. 삼각형의 두 변의 길이가 담긴 배열 sides, 나머지 한 변이 될 수 있는 정수의 개수를 반환하려면?
+function solution(sides) {
+    sides = sides.sort((a, b) => a - b);
+    const min = Math.abs(sides[0] - sides[1]) + 1;
+    const max = sides[0] + sides[1] - 1;
+    
+    return max - min + 1;
+}
