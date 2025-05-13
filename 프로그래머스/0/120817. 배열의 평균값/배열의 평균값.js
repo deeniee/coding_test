5ccb979fdb171d9a8dff04ed90f7208b6a7b2eb3
deeniee <dev.beeniee@gmail.com>
2025-05-14 01:54:05 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,3 +1,5 @@
+// Q. numbers의 원소의 평균값 구하기
+
 function solution(numbers) {
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
@@ -6,3 +8,10 @@ function solution(numbers) {
     const average = sum / numbers.length;
     return average;
 }
+
+// 다른 사람 풀이
+// function solution(numbers) {
+//     var answer = numbers.reduce((a,b) => a+b, 0) / numbers.length;
+//     return answer;
+// } >> reduce 메서드 : 배열의 여러개의 값을 하나의 값으로 축소
+
