389b317b2e87dbf37b292a3a0ac2e7d2b30c8bd8
deeniee <dev.beeniee@gmail.com>
2025-05-16 01:08:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -6,3 +6,11 @@ function solution(my_string) {
     };
     return sum;
 }
+
+// 다른 사람 풀이
+// function solution(my_string) {
+//     const answer = my_string.replace(/[^0-9]/g, '')
+//                             .split('')
+//                             .reduce((acc, curr) => acc + Number(curr), 0);
+//     return answer;
+// } >> reduce 함수로 배열의 요소를 순회하며 합계를 누적
