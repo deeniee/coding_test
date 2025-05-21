97d2671be92f67f02abd64b4f897cf632a540ed7
deeniee <dev.beeniee@gmail.com>
2025-05-21 11:19:26 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,9 @@
+function solution(num_list, n) {
+    var answer = [];
+    for (let i = 0; i < num_list.length; i+=n) {
+        let pair =[];
+        pair = num_list.slice(i,i+n);
+        answer.push(pair);
+    }
+    return answer;
+}
