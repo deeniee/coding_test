33458dbb121c732f92392d80265fef6a693fa2fc
deeniee <dev.beeniee@gmail.com>
2025-05-24 01:22:43 +0900

    [level 0] Title: 가까운 수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -6,3 +6,11 @@ function solution(array, n) {
         return diffA < diffB ? a : b;
     });
 }
+
+// 다른 사람 풀이
+// function solution(array, n) {
+//     array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+
+//     return array[0];
+// } >>> 절댓값을 활용해, 절댓값이 작은 수부터 오름차순으로 정렬 후, array의 0번 값을 반환
+//   >>> 절댓값을 활용했다는 공통점이 있지만 이 코드가 더욱 효율적임
