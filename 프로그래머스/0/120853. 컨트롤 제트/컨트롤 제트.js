e600cb5dd246e37b4c29367e91e32515cb5019d4
deeniee <dev.beeniee@gmail.com>
2025-05-26 00:41:02 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -4,3 +4,15 @@ function solution(s) {
     let sum = numArray.reduce((acc,cur)=>acc+cur)
     return sum;
 }
+
+// 다른 사람 풀이
+// function solution(s) {
+//     const stack = []
+
+//     s.split(' ').forEach((target) => {
+//         if(target === 'Z') stack.pop();
+//         else stack.push(+target)
+//     })
+
+//     return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+// }
