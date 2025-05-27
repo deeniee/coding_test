50f1dd0502f3b26a6eadaf8547c7412f243b6594
deeniee <dev.beeniee@gmail.com>
2025-05-27 19:54:18 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let arr = my_string.split(' ')
+    let cal = arr[0] * 1;
+    for(let i = 1; i< arr.length; i++){
+        if(arr[i] === '+')
+            cal += arr[i+1] * 1;
+        if(arr[i] === '-')
+            cal -= arr[i+1] * 1;
+    }
+    return cal;
+}
