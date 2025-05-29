eaedae35ca030f3a5a0e40bb19b6b6edef17af16
deeniee <dev.beeniee@gmail.com>
2025-05-29 20:18:20 +0900

    [level 0] Title: 등수 매기기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -5,3 +5,25 @@ function solution(score) {
     
     return sum.map(x => order.indexOf(x)+1);
 }
+
+// 다른 사람 풀이
+// function solution(score) {
+//   return score.map((el) => {
+//     return (
+//       score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+//     );
+//   });
+// }
+
+// chatGPT 코드
+// function solution(score) {
+//     const avg = score.map(([a, b]) => (a + b) / 2);
+//     const sorted = [...avg].sort((a, b) => b - a);
+//     const rankMap = new Map();
+
+//     sorted.forEach((v, i) => {
+//         if (!rankMap.has(v)) rankMap.set(v, i + 1);
+//     });
+
+//     return avg.map(v => rankMap.get(v));
+// } >> indexOf 대신 Map으로 최적화
