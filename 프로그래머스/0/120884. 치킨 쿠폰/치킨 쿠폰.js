9e714900871e4b7ea516783903d92182e627733c
deeniee <dev.beeniee@gmail.com>
2025-05-30 23:04:22 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -10,3 +10,9 @@ function solution(chicken) {
     }
     return totalService;
 }
+
+// 다른 사람 풀이
+// function solution(chicken) {
+//     var answer = parseInt((chicken-1) / 9);
+//     return answer;
+// } >> 첫 쿠폰을 받기 위해선 10마리를 시켜야한다. 그 후 쿠폰 1장이 있는 상태로 9마리만 시키면 서비스 치킨을 시킬 수 있으니, 9로 나누면 서비스 치킨 수를 셀 수 있다. 
