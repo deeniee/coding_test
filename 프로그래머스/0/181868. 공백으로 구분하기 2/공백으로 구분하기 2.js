8642ef53529431841f2f0f30558dd0bbfc34fd53
deeniee <dev.beeniee@gmail.com>
2025-07-21 23:01:22 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,4 +1,7 @@
-function solution(my_string) {
-    
+function solution(my_string) {  
     return my_string.split(' ').filter(v => v.length !== 0);
 }
+
+// function solution(my_string) {  
+//     return my_string.trim().split(/\s+/);
+// } >> 정규표현식 활용, 앞뒤 공백 제거를 위해 trim 활용
