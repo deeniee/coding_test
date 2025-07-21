90edb542475a75ac8761896dbe0a4351db862cc8
deeniee <dev.beeniee@gmail.com>
2025-07-21 23:05:05 +0900

    [level 0] Title: rny_string, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,3 +1,7 @@
 function solution(rny_string) {
     return [...rny_string].map(v => v === 'm' ? 'rn' : v).join('');
 }
+
+// function solution(rny_string) {
+//     return rny_string.replaceAll(/m/g, "rn");
+// } >> 정규표현식 활용
