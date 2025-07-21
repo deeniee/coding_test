5197fccc87375d1257ad674a7e633ddb940f75af
deeniee <dev.beeniee@gmail.com>
2025-07-21 19:00:09 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,8 @@
+function solution(my_string, is_suffix) {
+    var suffixs = [];
+    for (let i = 0; i < my_string.length; i++) {
+        suffixs.push(my_string.slice(i))
+    }
+    
+    return suffixs.includes(is_suffix) ? 1 : 0;
+}
