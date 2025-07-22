cbf0a3f23ec49b616496ec91e0ae1d8b90d28aaf
deeniee <dev.beeniee@gmail.com>
2025-07-22 16:33:51 +0900

    [level 0] Title: 세로 읽기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -7,3 +7,7 @@ function solution(my_string, m, c) {
 
     return rows.map(row => row[c - 1]).join('');
 }
+
+// function solution(my_string, m, c) {
+//     return [...my_string].filter((_, i) => i % m === c - 1).join('');
+// }
