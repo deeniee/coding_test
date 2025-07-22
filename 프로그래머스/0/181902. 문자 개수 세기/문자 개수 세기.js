0e778ef0b43864e897db30b6d700d088ecbafb6d
deeniee <dev.beeniee@gmail.com>
2025-07-22 17:20:24 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,16 @@
+function solution(my_string) {
+    const result = new Array(52).fill(0);
+
+    for (let char of my_string) {
+        const code = char.charCodeAt(0);
+        if (code >= 65 && code <= 90) {
+            // A~Z
+            result[code - 65]++;
+        } else if (code >= 97 && code <= 122) {
+            // a~z
+            result[code - 97 + 26]++;
+        }
+    }
+
+    return result;
+}
