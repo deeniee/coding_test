b024485410547d473930d89f48c9f9d384589c8f
deeniee <dev.beeniee@gmail.com>
2025-07-23 22:30:52 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,19 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer;
+
+    if (n === 1) {
+        return num_list.slice(0, b + 1);
+    } 
+    if (n === 2) {
+        return num_list.slice(a);
+    } 
+    if (n === 3) {
+        return num_list.slice(a, b + 1);
+    } 
+    // n === 4
+    const result = [];
+    for (let i = a; i <= b; i += c) {
+        result.push(num_list[i]);
+    }
+    return result;
+}
