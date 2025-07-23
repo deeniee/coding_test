74cde35eb88b6d874e6486ddf8cb72d714eb1b44
deeniee <dev.beeniee@gmail.com>
2025-07-23 17:49:48 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,8 @@
+function solution(date1, date2) {
+    let [y1, m1, d1] = date1;
+    let [y2, m2, d2] = date2;
+    
+    if (y1 !== y2) return y1 < y2 ? 1 : 0;
+    if (m1 !== m2) return m1 < m2 ? 1 : 0;
+    return d1 < d2 ? 1 : 0;
+}
