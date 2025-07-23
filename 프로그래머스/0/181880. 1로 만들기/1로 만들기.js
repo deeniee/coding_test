2cb51ffc86fd5f9da12228b753eaf52fa7a60a11
deeniee <dev.beeniee@gmail.com>
2025-07-23 21:26:22 +0900

    [level 0] Title: 1로 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    var count = 0;
+    for (let num of num_list) {
+        while (num > 1) {
+            if (num % 2 === 0) {
+                num /= 2
+                count++;
+            } else {
+                num = (num-1)/2
+                count++;
+            }
+        }
+    }
+    return count;
+}
