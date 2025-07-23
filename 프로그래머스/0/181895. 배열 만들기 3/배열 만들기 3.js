59bfc2ede6e05170be6e741ef02437857e20271a
deeniee <dev.beeniee@gmail.com>
2025-07-23 13:39:39 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.95 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -5,3 +5,9 @@ function solution(arr, intervals) {
     }
     return result;
 }
+
+// function solution(arr, intervals) {
+//     const [[a,b],[c,d]] = intervals;
+
+//     return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
+// }
