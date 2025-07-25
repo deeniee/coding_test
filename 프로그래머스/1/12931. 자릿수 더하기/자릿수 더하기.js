7ad55d48606a3e1c4acc25d44d0f1bace4728327
deeniee <dev.beeniee@gmail.com>
2025-07-25 18:38:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let str = n.toString()
+    let arr = []
+    for (let i = 0; i < str.length; i++) {
+        arr.push(Number(str[i]))
+        
+    }
+    return arr.reduce((a,b)=>a+b)
+}
