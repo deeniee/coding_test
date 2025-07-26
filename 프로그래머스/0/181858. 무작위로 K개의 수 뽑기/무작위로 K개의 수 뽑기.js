1a5595fd99c05a2b4c6a46df1002f3fdc6dde901
deeniee <dev.beeniee@gmail.com>
2025-07-27 01:43:16 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 603.18 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -9,3 +9,24 @@ function solution(arr, k) {
     
     return sorted.length >= k ? sorted.slice(0,k) : sorted.concat(Array(diff).fill(-1));
 }
+
+// function solution(arr, k) {
+//     const sorted = [];
+//     const seen = new Set();
+
+//     for (const num of arr) {
+//         if (!seen.has(num)) {
+//             seen.add(num);
+//             sorted.push(num);
+//             if (sorted.length === k) break;
+//         }
+//     }
+
+//     while (sorted.length < k) {
+//         sorted.push(-1);
+//     }
+
+//     return sorted;
+// }
+
+
