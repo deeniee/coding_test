984803eb90ab03020f5a131adfef6f19b22028a4
deeniee <dev.beeniee@gmail.com>
2025-07-27 01:28:59 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -9,3 +9,11 @@ function solution(rank, attendance) {
     let [a,b,c] = sorted;
     return 10000*rank.indexOf(a) + 100*rank.indexOf(b) + rank.indexOf(c);
 }
+
+// function solution(rank, attendance) {
+//   const [a, b, c] = rank
+//     .map((r, i) => [r, i])
+//     .filter(([_, i]) => attendance[i])
+//     .sort(([a], [b]) => a - b);
+//   return 10000 * a[1] + 100 * b[1] + c[1];
+// }
