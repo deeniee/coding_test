a9656fa9962b7dd413acd0e3a24ffd6e4cbede8a
deeniee <dev.beeniee@gmail.com>
2025-07-27 16:30:50 +0900

    [level 0] Title: 그림 확대, Time: 1.53 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -8,3 +8,17 @@ function solution(picture, k) {
     }
     return newPic;
 }
+
+// 개선된 코드
+// function solution(picture, k) {
+//     const newPic = [];
+
+//     for (const row of picture) { 
+//         const scaledRow = [...row].map(char => char.repeat(k)).join('');
+//         for (let i = 0; i < k; i++) {
+//             newPic.push(scaledRow);
+//         }
+//     }
+
+//     return newPic;
+// } >> picture의 각 요소만 반복하면 되니, 간단하게 for of문 사용
