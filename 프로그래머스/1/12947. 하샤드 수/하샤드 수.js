054c2d9fb45f8faeddf53b903f48ba44d649a45f
deeniee <dev.beeniee@gmail.com>
2025-07-28 15:01:56 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -3,3 +3,14 @@ function solution(x) {
     let sum = arr.reduce((a,b)=>a+b);
     return x%sum === 0 ? true : false;
 }
+
+// function solution(x) {
+//     let num = x;
+//     let sum = 0;
+//     do {
+//         sum += x%10;
+//         x = Math.floor(x/10);
+//     } while (x>0);
+
+//     return !(num%sum);
+// }
