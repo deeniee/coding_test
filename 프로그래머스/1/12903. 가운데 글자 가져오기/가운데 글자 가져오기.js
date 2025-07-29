d49f22f02ce76e03b7814108a14bf75bf99cc122
deeniee <dev.beeniee@gmail.com>
2025-07-29 21:17:07 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,6 +1,11 @@
 function solution(s) {
-    var answer = '';
     let len = s.length;
     let center = (len-1)/2;
     return len%2===0 ? [...s].slice(center,center+2).join('') : [...s][center];
 }
+
+// function solution(s) {
+//   return s.length % 2 === 0
+//     ? s.slice(s.length / 2 - 1, s.length / 2 + 1)
+//     : s[Math.floor(s.length / 2)];
+// }
