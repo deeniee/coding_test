820c7bfb80b0057d9cedc7bf5b3b06e7f1f25873
deeniee <dev.beeniee@gmail.com>
2025-08-01 17:12:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 27.19 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -14,3 +14,12 @@ function solution(left, right) {
 
     return sum;
 }
+
+// 약수의 개수가 홀수인 수는 제곱수일 때뿐이라는 수학적 성질을 활용
+// function solution(left, right) {
+//     let sum = 0;
+//     for (let i = left; i <= right; i++) {
+//         Math.sqrt(i) % 1 === 0 ? sum -= i : sum += i;
+//     }
+//     return sum;
+// }
